f45d5c13d59e1b5a269af3baad423b5e6efea15f
Sinetheta <k.attfield@gmail.com>
2016-12-03 13:58:43 -0800

    Add 3by3 slide breakpoint

--- source/assets/scripts/pages/index/tour_section.js
+++ source/assets/scripts/pages/index/tour_section.js
@@ -1,8 +1,17 @@
+const MQ_SLIDES_3_BY_3 = 600; // must match $breakpoint-tour-3-slides
 let Flickity = require('flickity');
 require("flickity/dist/flickity.css");
+var pageWidth = $(window).width();
+var groupCells, pageDots, prevNextButtons;
+
+if (pageWidth >= MQ_SLIDES_3_BY_3 ) {
+  groupCells = 3;
+} else {
+  groupCells = 2;
+}
 
 var flkty = new Flickity( $('.features')[0], {
-  groupCells: 2,
+  groupCells: groupCells,
   pageDots: true,
   prevNextButtons: false,
   wrapAround: true,
