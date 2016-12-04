66c0ab8a11f86ef2d002b7f54e60f34bc2bc18e8
Sinetheta <k.attfield@gmail.com>
2016-12-03 22:41:26 -0800

    Advance scroll slider based on age scroll

--- source/assets/scripts/components/scroll_slider.js
+++ source/assets/scripts/components/scroll_slider.js
@@ -1,4 +1,5 @@
-$('.scroll-slider .slide').first().addClass('active');
+var throttle = require('throttle-debounce/throttle');
+var sliders = [];
 
 class ScrollSlider {
   static init() {
@@ -7,10 +8,24 @@ class ScrollSlider {
     });
   }
 
+  static checkScroll() {
+    var scrollTop = $(window).scrollTop();
+    for (var slider of sliders) {
+      let progress = slider.progress(scrollTop);
+      let slide = Math.floor(progress * slider.$slides.length);
+      slide = Math.min(slide, slider.$slides.length - 1)
+      console.log(slide)
+      slider.select(slide);
+    }
+  }
+
   constructor(el) {
+    sliders.push(this);
     this.$el = $(el).addClass('scroll-slider');
     this.$el.data('slider-instance', this);
     this.name = this.$el.data('scroll-slider');
+    this.$nav = $(`[data-scroll-slider-nav-for="${this.name}"]`);
+    this.$trigger = $(`[data-scroll-trigger-for="${this.name}"]`);
     this.$slides = this.$el.children();
     this.$slides.wrapAll('<div class="slides">');
     this.createNav();
@@ -18,14 +33,15 @@ class ScrollSlider {
   }
 
   createNav() {
-    this.$nav = $(`[data-scroll-slider-nav-for="${this.name}"]`)
-      .addClass('scroll-slider-nav');
+    this.$nav.addClass('scroll-slider-nav');
     this.$slides.each( (i) => {
       $('<li/>').appendTo(this.$nav);
     });
   }
 
   select(slideIndex) {
+    if (slideIndex == this.slideIndex) return;
+    this.slideIndex = slideIndex;
     this.$nav.find('li')
       .removeClass('active')
       .eq(slideIndex).addClass('active');
@@ -33,8 +49,17 @@ class ScrollSlider {
       .removeClass('active')
       .eq(slideIndex).addClass('active');
   }
+
+  progress(scrollTop) {
+    let $container = this.$trigger.parent();
+    let track = $container.height() - this.$trigger.height();
+    let distance = this.$trigger.offset().top - $container.offset().top;
+    return distance / track;
+  }
 }
 
 ScrollSlider.init();
+$(window).on("scroll", throttle(50, ScrollSlider.checkScroll));
+
 
 module.exports = ScrollSlider;
--- source/assets/scripts/pages/index/feature_section.js
+++ source/assets/scripts/pages/index/feature_section.js
@@ -1,1 +1,7 @@
+const TOPNAV_HEIGHT = 95; // must match $topnav-height
+require('vendor/jquery.sticky-kit');
 var ScrollSlider = require('components/scroll_slider');
+
+$('.features-section .sticky-slider').stick_in_parent({
+  offset_top: TOPNAV_HEIGHT
+});
