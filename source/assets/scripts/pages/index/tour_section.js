7c9e3324429a42f9aa5f356ae896749dc32bfcca
Sinetheta <k.attfield@gmail.com>
2016-12-18 15:24:44 -0800

    Show page dots on all sizes

--- source/assets/scripts/pages/index/tour_section.js
+++ source/assets/scripts/pages/index/tour_section.js
@@ -7,21 +7,17 @@ var groupCells, pageDots, prevNextButtons;
 
 if (pageWidth >= MQ_SLIDES_4_BY_4 ) {
   groupCells = 4;
-  pageDots = false;
   prevNextButtons = true;
 } else if (pageWidth >= MQ_SLIDES_3_BY_3 ) {
   groupCells = 3;
-  pageDots = true;
   prevNextButtons = false;
 } else {
   groupCells = 2;
-  pageDots = true;
   prevNextButtons = false;
 }
 
 var flkty = new Flickity( $('.features')[0], {
   groupCells: groupCells,
-  pageDots: pageDots,
   prevNextButtons: prevNextButtons,
   wrapAround: true,
 });
