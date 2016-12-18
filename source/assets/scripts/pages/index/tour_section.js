bc72c443ba97dd9ebd349454e8dd7f5770a0d6a7
Sinetheta <k.attfield@gmail.com>
2016-12-18 15:24:54 -0800

    Show prev next buttons on small screens

--- source/assets/scripts/pages/index/tour_section.js
+++ source/assets/scripts/pages/index/tour_section.js
@@ -7,17 +7,13 @@ var groupCells, pageDots, prevNextButtons;
 
 if (pageWidth >= MQ_SLIDES_4_BY_4 ) {
   groupCells = 4;
-  prevNextButtons = true;
 } else if (pageWidth >= MQ_SLIDES_3_BY_3 ) {
   groupCells = 3;
-  prevNextButtons = false;
 } else {
   groupCells = 2;
-  prevNextButtons = false;
 }
 
 var flkty = new Flickity( $('.features')[0], {
   groupCells: groupCells,
-  prevNextButtons: prevNextButtons,
   wrapAround: true,
 });
