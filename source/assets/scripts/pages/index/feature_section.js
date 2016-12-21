0903dd3dee4acac42d4cb712d742f8aff08b7c92
Sinetheta <k.attfield@gmail.com>
2016-12-20 21:19:44 -0800

    Same size for all slide

--- source/assets/scripts/pages/index/feature_section.js
+++ source/assets/scripts/pages/index/feature_section.js
@@ -5,12 +5,22 @@ let $slider = $('.features-slider');
 let flkty = new Flickity( $slider[0], {
   wrapAround: true,
   initialIndex: 3
-})
+});
 let updateSelectedClass = function() {
   let lastClass = $slider.data('lastClass');
   $slider.removeClass(`selected-${lastClass}`)
     .addClass(`selected-${flkty.selectedIndex}`)
     .data('lastClass', flkty.selectedIndex);
 };
+let homogeonizeTextContainers = function() {
+  let textContainers = $slider.find('.text-container');
+  let maxHeight = textContainers.map(function() {
+      return $(this).height();
+    }).toArray().sort().reverse()[0];
+  textContainers.each(function() {
+    $(this).height(maxHeight);
+  });
+};
 flkty.on('select', updateSelectedClass);
 updateSelectedClass();
+homogeonizeTextContainers();
