98ccd8d88b6cc1f1c865511bec6718cc1906ecd8
David Piechnik <dpiechnik@gmail.com>
2016-12-22 15:03:15 -0700

    Add refresh on resize
    Sorry, Kevin.

--- source/assets/scripts/pages/index.js
+++ source/assets/scripts/pages/index.js
@@ -4,3 +4,13 @@ require('./index/scrolled_past_top')
 require('./index/scroll_link')
 require('./index/tour_section')
 require('./index/feature_section')
+
+// Sorry, Kevin.
+var lastWidth = $(window).width();
+
+$(window).resize(function(){
+   if($(window).width()!=lastWidth){
+      location.reload();
+      lastWidth = $(window).width();
+   }
+})
