775793969bc8b1790e3b1ac526a37863f82b95a9
David Piechnik <dpiechnik@gmail.com>
2016-12-22 15:03:02 -0700

    Fixed tour slides

--- source/assets/scripts/pages/index/tour_section.js
+++ source/assets/scripts/pages/index/tour_section.js
@@ -1,5 +1,5 @@
 const MQ_SLIDES_3_BY_3 = 600; // must match $breakpoint-tour-3-slides
-const MQ_SLIDES_4_BY_4 = 800; // must match $breakpoint-tour-4-slides
+const MQ_SLIDES_4_BY_4 = 960; // must match $breakpoint-tour-4-slides
 let Flickity = require('flickity');
 require("flickity/dist/flickity.css");
 var pageWidth = $(window).width();
